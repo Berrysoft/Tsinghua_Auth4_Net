8cd2120b021e5436c3903c56dea91396d025af39
王宇逸 <Strawberry_Str@hotmail.com>
2018-04-19 15:59:11 +0800

    有线网正常下线（仍有延迟）

--- TsinghuaNet/Auth4Helper.vb
+++ TsinghuaNet/Auth4Helper.vb
@@ -1,11 +1,11 @@
 ﻿Class Auth4Helper
     Inherits NetHelperBase
     Private Const Host = "auth4.tsinghua.edu.cn"
-    Private Const ConnectUrl = "http://auth4.tsinghua.edu.cn/srun_portal_pc.php?ac_id=1&"
-    Private Const LogOutUrl = "http://auth4.tsinghua.edu.cn/include/auth_action.php"
+    Private Const ConnectUrl = "http://auth4.tsinghua.edu.cn/srun_portal_pc.php?"
+    Private Const LogOutUrl = "http://auth4.tsinghua.edu.cn/cgi-bin/srun_portal"
     Private Const FluxUrl = "http://auth4.tsinghua.edu.cn/rad_user_info.php"
     Private Const ConnectData = "action=login&ac_id=1&user_ip=&nas_ip=&user_mac=&url=&username={0}&password={1}&save_me=0"
-    Private Const LogOutData = "action=logout&username={0}&password={1}&ajax=1"
+    Private Const LogOutData = "action=logout&ac_id=1&ip=&double_stack=1"
 
     Public Sub New(username As String, password As String)
         MyBase.New(username, password)
@@ -22,7 +22,7 @@
     End Function
 
     Public Overrides Async Function LogOut() As Task(Of String)
-        Return (Await Post(LogOutUrl, String.Format(LogOutData, Username, Password))).ErrorMessage
+        Return (Await Post(LogOutUrl, LogOutData)).ErrorMessage
     End Function
 
     Public Overrides Async Function GetFlux() As Task(Of (Response As String, ErrorMessage As String))
