5def4f5ba4361dbf6c0121ad23bac7aee629fcb4
王宇逸 <Strawberry_Str@hotmail.com>
2018-04-21 22:07:54 +0800

    添加日语、朝鲜语

--- TsinghuaNet/MainViewModel.vb
+++ TsinghuaNet/MainViewModel.vb
@@ -118,3 +118,20 @@ Class FluxToString
         Throw New NotImplementedException()
     End Function
 End Class
+
+Class NullableToVisibility
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+        Dim source As Long? = value
+        If source.HasValue Then
+            Return Visibility.Visible
+        Else
+            Return Visibility.Collapsed
+        End If
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Throw New NotImplementedException()
+    End Function
+End Class
