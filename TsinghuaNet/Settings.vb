de9a94fdb27528990fc211e20147b89232252a33
王宇逸 <Strawberry_Str@hotmail.com>
2018-05-15 17:43:31 +0800

    使用Berrysoft.Console.XmlSettings改写Settings类

--- TsinghuaNet/Settings.vb
+++ TsinghuaNet/Settings.vb
@@ -1,55 +1,32 @@
 ﻿Imports System.Globalization
 Imports System.Net.NetworkInformation
 Imports System.Text
-Imports Berrysoft.Tsinghua.Net
+Imports Berrysoft.Console
 
+<Settings("user")>
 Class Settings
+    Inherits XmlSettings
+    <Settings("name")>
+    Public Property Username As String
+    <Settings("password")>
+    Public Property Password As String
+    <Settings("state")>
+    Public Property State As NetState
+    <Settings("more")>
+    Public Property MoreInf As Boolean
+    <Settings("language")>
+    Public Property Language As CultureInfo
     Private Const logPath As String = "log.xml"
     Public Sub New()
-        InitLog()
-    End Sub
-    Private Async Sub InitLog()
-        Try
-            Dim logFile As XDocument = XDocument.Load(logPath)
-            Username = If(logFile.<user>.<name>.Value, String.Empty)
-            Dim passwordstr As String = logFile.<user>.<password>.Value
-            If passwordstr Is Nothing Then
-                Password = String.Empty
-            Else
-                Password = Encoding.ASCII.GetString(Convert.FromBase64String(passwordstr))
-            End If
-            Dim statestr As String = logFile.<user>.<state>.Value
-            If statestr Is Nothing Then
-                State = Await GetConnectableStateAsync()
-            Else
-                Dim temp As NetState
-                If [Enum].TryParse(statestr, temp) Then
-                    State = temp
-                Else
-                    State = Await GetConnectableStateAsync()
-                End If
-            End If
-            Dim moreinf As String = logFile.<user>.<more>.Value
-            Dim moreinfResult As Boolean
-            If Boolean.TryParse(moreinf, moreinfResult) AndAlso moreinfResult Then
-                Me.MoreInf = True
-            End If
-            Dim lang As String = logFile.<user>.<language>.Value
-            If lang IsNot Nothing Then
-                Try
-                    Language = New CultureInfo(lang)
-                Catch ex As Exception
-
-                End Try
-            End If
-        Catch ex As Exception
-            InitBlankLog()
-        End Try
-    End Sub
-    Private Async Sub InitBlankLog()
         Username = String.Empty
         Password = String.Empty
-        State = Await GetConnectableStateAsync()
+        Me.OpenAsync()
+    End Sub
+    Private Async Sub OpenAsync()
+        Open(logPath)
+        If State = NetState.Unknown Then
+            State = Await GetConnectableStateAsync()
+        End If
     End Sub
     Private Async Function GetConnectableStateAsync() As Task(Of NetState)
         Dim ping As New Ping()
@@ -60,23 +37,45 @@ Class Settings
             Return NetState.Net
         End If
     End Function
-    Public Sub Save()
-        Dim log As XDocument =
-            <?xml version="1.0" encoding="utf-8"?>
-            <user>
-                <name><%= Username %></name>
-                <password><%= Convert.ToBase64String(Encoding.ASCII.GetBytes(Password)) %></password>
-                <state><%= State.ToString() %></state>
-                <more><%= MoreInf.ToString() %></more>
-            </user>
-        If Language IsNot Nothing Then
-            log.Element("user").Add(New XElement("language", Language.Name))
-        End If
-        log.Save(logPath)
+    Public Overloads Sub Save()
+        Save(logPath)
     End Sub
-    Public Property Username As String
-    Public Property Password As String
-    Public Property State As NetState
-    Public Property MoreInf As Boolean
-    Public Property Language As CultureInfo
+    Protected Overrides Function ChangeType(name As XName, value As Object, conversionType As Type) As Object
+        Select Case name
+            Case "password"
+                Return Encoding.ASCII.GetString(Convert.FromBase64String(value))
+            Case "state"
+                Dim temp As NetState
+                If [Enum].TryParse(value, temp) Then
+                    Return temp
+                Else
+                    Return NetState.Unknown
+                End If
+            Case "more"
+                Dim moreinfResult As Boolean
+                If Boolean.TryParse(value, moreinfResult) AndAlso moreinfResult Then
+                    Return True
+                Else
+                    Return False
+                End If
+            Case "language"
+                Try
+                    Return New CultureInfo(value.ToString())
+                Catch ex As Exception
+                    Return Nothing
+                End Try
+            Case Else
+                Return value
+        End Select
+    End Function
+    Protected Overrides Function ChangeBackType(name As XName, value As Object, conversionType As Type) As Object
+        Select Case name
+            Case "password"
+                Return Convert.ToBase64String(Encoding.ASCII.GetBytes(value))
+            Case "language"
+                Return CType(value, CultureInfo)?.Name
+            Case Else
+                Return value.ToString()
+        End Select
+    End Function
 End Class
