702fc89bec6ee496480c0e9497206226c2241fff
王宇逸 <Strawberry_Str@hotmail.com>
2018-11-14 12:24:43 +0800

    发布新版

--- TsinghuaNet/My Project/AssemblyInfo.vb
+++ TsinghuaNet/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' 方法是按如下所示使用“*”: :
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("2.5.106.0")>
-<Assembly: AssemblyFileVersion("2.5.106.0")>
+<Assembly: AssemblyVersion("2.6.111.0")>
+<Assembly: AssemblyFileVersion("2.6.111.0")>
